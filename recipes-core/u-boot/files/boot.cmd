2c3c27429d90c47f9c4c8856dec745683dfc2346
Alexander Sack <asac@pantacor.com>
2023-07-05 12:33:38 +0200

    fit support for boot.cmd

--- recipes-core/u-boot/files/boot.cmd
+++ recipes-core/u-boot/files/boot.cmd
@@ -1,9 +1,9 @@
 setenv pv_baseargs "root=/dev/ram rootfstype=ramfs rdinit=/usr/bin/pantavisor pv_storage.device=LABEL=root pv_storage.fstype=ext4"
 setenv pv_mmcdev ${mmcdev}
-setenv pv_mmcboot 0
 setenv pv_ctrl 1
 setenv devtype mmc
 setenv envloadaddr ${loadaddr}
+setenv distro_bootpart 1
 part size ${devtype} ${pv_mmcdev} ${pv_ctrl} pv_config_size
 if test "${pv_config_size}" = "800"; then
 	echo Found PV OEM Config at ${devtype} ${pv_mmcdev}:2
@@ -12,14 +12,19 @@ if test "${pv_config_size}" = "800"; then
 	env import ${envloadaddr} 0x1000
 	setexpr pv_mmcdata ${pv_ctrl} + 1
 else
-	setexpr pv_mmcdata ${pv_mmcboot} + 1
+	setexpr pv_mmcdata ${distro_bootpart} + 1
 fi
+
+echo "pv_mmcdata selected = ${pv_mmcdata}"
+
+echo Loading uboot.txt
+echo load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${envloadaddr} /boot/uboot.txt; setenv uboot_txt_size ${filesize}
 load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${envloadaddr} /boot/uboot.txt; setenv uboot_txt_size ${filesize}
 
 echo Loading pantavisor uboot.txt
 setenv pv_try; env import ${envloadaddr} ${uboot_txt_size}
 
-load ${devtype} ${pv_mmcdev}:${pv_mmcboot} ${envloadaddr} pv.env; setenv pv_env_size ${filesize}
+load ${devtype} ${pv_mmcdev}:${distro_bootpart} ${envloadaddr} pv.env; setenv pv_env_size ${filesize}
 echo Loading pv.env
 setenv pv_trying; env import ${envloadaddr} ${pv_env_size}
 
@@ -29,18 +34,29 @@ if env exists pv_try; then
 		echo Pantavisor boots checkpoint revision ${pv_rev} after failed try-boot of revision: ${pv_try}
 		setenv pv_trying
 		env export ${envloadaddr} pv_trying; setenv pv_env_size ${filesize};
-		save ${devtype} ${pv_mmcdev}:${pv_mmcboot} ${envloadaddr} pv.env ${pv_env_size}
+		save ${devtype} ${pv_mmcdev}:${distro_bootpart} ${envloadaddr} pv.env ${pv_env_size}
 		setenv boot_rev ${pv_rev}
 	else
 		echo Pantavisor boots try-boot revision: ${pv_try}
 		setenv pv_trying ${pv_try}
 		env export ${envloadaddr} pv_trying; setenv pv_env_size ${filesize};
-		save ${devtype} ${pv_mmcdev}:${pv_mmcboot} ${envloadaddr} pv.env ${pv_env_size}
+		save ${devtype} ${pv_mmcdev}:${distro_bootpart} ${envloadaddr} pv.env ${pv_env_size}
 		setenv boot_rev ${pv_trying}
 	fi
 else
 	echo Pantavisor boots revision: ${pv_rev}
 	setenv boot_rev ${pv_rev}
+fi
+
+if load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${addr_fit} /trails/${boot_rev}/bsp/pantavisor.fit; then
+	echo Successfully loaded pantavisor.fit. booting...
+        iminfo ${addr_fit}
+	run findfdt
+	run get_fit_config
+        bootm ${addr_fit}#${name_fit_config}
+	echo "failed to boot fit ... resetting in 5 sec"
+	sleep 5
+	reset
 fi
 
 echo Pantavisor loading FDT at ${fdt_addr_r}
@@ -57,10 +73,10 @@ echo Pantavisor bootargs: "${pv_baseargs} pv_try=${pv_try} pv_rev=${boot_rev} pa
 setenv bootargs "${pv_baseargs} pv_try=${pv_try} pv_rev=${boot_rev} panic=2 ${fdtbootargs} ${configargs} ${localargs}"
 echo Pantavisor kernel load: load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${kernel_addr_r} /trails/${boot_rev}/.pv/pv-kernel.img
 load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${kernel_addr_r} /trails/${boot_rev}/.pv/pv-kernel.img
-echo Pantavisor initrd load: load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${initrd_addr} /trails/${boot_rev}/.pv/pv-initrd.img
-load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${initrd_addr} /trails/${boot_rev}/.pv/pv-initrd.img
+echo Pantavisor initrd load: load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${ramdisk_addr_r} /trails/${boot_rev}/.pv/pv-initrd.img
+load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${ramdisk_addr_r} /trails/${boot_rev}/.pv/pv-initrd.img
 setenv rd_size ${filesize}
-setexpr rd_offset ${initrd_addr} + ${rd_size}
+setexpr rd_offset ${ramdisk_addr_r} + ${rd_size}
 setenv i 0
 while load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${rd_offset} /trails/${boot_rev}/.pv/pv-initrd.img.${i}; do
 	echo Pantavisor initrd addon loaded: load ${devtype} ${mmcdev}:${mmcdata} ${rd_offset} /trails/${boot_rev}/.pv/pv-initrd.img.${i}
@@ -68,7 +84,7 @@ while load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${rd_offset} /trails/${boot_rev
 	setexpr rd_size ${rd_size} + ${filesize}
 	setexpr rd_offset ${rd_offset} + ${filesize}
 done
-echo "Pantavisor go... : booti ${kernel_addr_r} ${initrd_addr}:${rd_size} ${fdt_addr_r}"
-booti ${kernel_addr_r} ${initrd_addr}:${rd_size} ${fdt_addr_r}
+echo "Pantavisor go... : booti ${kernel_addr_r} ${ramdisk_addr_r}:${rd_size} ${fdt_addr_r}"
+booti ${kernel_addr_r} ${ramdisk_addr_r}:${rd_size} ${fdt_addr_r}
 echo "Failed to boot step, rebooting"; sleep 5; reset
 
