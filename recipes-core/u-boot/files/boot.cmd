a0e1848c4cbf564fd73563c412884c64d379ac17
Alexander Sack <asac@pantacor.com>
2023-07-28 13:25:24 +0200

    overhaul our default boot.cmd script to support fit images and be smarter about device:part selection

--- recipes-core/u-boot/files/boot.cmd
+++ recipes-core/u-boot/files/boot.cmd
@@ -1,9 +1,17 @@
 setenv pv_baseargs "root=/dev/ram rootfstype=ramfs rdinit=/usr/bin/pantavisor pv_storage.device=LABEL=root pv_storage.fstype=ext4"
-setenv pv_mmcdev ${mmcdev}
 setenv pv_ctrl 1
 setenv devtype mmc
 setenv envloadaddr ${loadaddr}
-setenv distro_bootpart 1
+
+pv_mmcdev=${devnum}
+if test -z "${devnum}"; then
+	pv_mmcdev=1
+fi
+pv_bootpart=${distro_bootpart}
+if test -z "${pv_bootpart}"; then
+	pv_bootpart=1
+fi
+
 part size ${devtype} ${pv_mmcdev} ${pv_ctrl} pv_config_size
 if test "${pv_config_size}" = "800"; then
 	echo Found PV OEM Config at ${devtype} ${pv_mmcdev}:2
@@ -12,10 +20,10 @@ if test "${pv_config_size}" = "800"; then
 	env import ${envloadaddr} 0x1000
 	setexpr pv_mmcdata ${pv_ctrl} + 1
 else
-	setexpr pv_mmcdata ${distro_bootpart} + 1
+	setexpr pv_mmcdata ${pv_bootpart} + 1
 fi
 
-echo "pv_mmcdata selected = ${pv_mmcdata}"
+echo "mmc root selected = ${pv_mmcdev}:${pv_mmcdata}"
 
 echo Loading uboot.txt
 echo load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${envloadaddr} /boot/uboot.txt; setenv uboot_txt_size ${filesize}
@@ -24,7 +32,7 @@ load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${envloadaddr} /boot/uboot.txt; seten
 echo Loading pantavisor uboot.txt
 setenv pv_try; env import ${envloadaddr} ${uboot_txt_size}
 
-load ${devtype} ${pv_mmcdev}:${distro_bootpart} ${envloadaddr} pv.env; setenv pv_env_size ${filesize}
+load ${devtype} ${pv_mmcdev}:${pv_bootpart} ${envloadaddr} pv.env; setenv pv_env_size ${filesize}
 echo Loading pv.env
 setenv pv_trying; env import ${envloadaddr} ${pv_env_size}
 
@@ -34,13 +42,13 @@ if env exists pv_try; then
 		echo Pantavisor boots checkpoint revision ${pv_rev} after failed try-boot of revision: ${pv_try}
 		setenv pv_trying
 		env export ${envloadaddr} pv_trying; setenv pv_env_size ${filesize};
-		save ${devtype} ${pv_mmcdev}:${distro_bootpart} ${envloadaddr} pv.env ${pv_env_size}
+		save ${devtype} ${pv_mmcdev}:${pv_bootpart} ${envloadaddr} pv.env ${pv_env_size}
 		setenv boot_rev ${pv_rev}
 	else
 		echo Pantavisor boots try-boot revision: ${pv_try}
 		setenv pv_trying ${pv_try}
 		env export ${envloadaddr} pv_trying; setenv pv_env_size ${filesize};
-		save ${devtype} ${pv_mmcdev}:${distro_bootpart} ${envloadaddr} pv.env ${pv_env_size}
+		save ${devtype} ${pv_mmcdev}:${pv_bootpart} ${envloadaddr} pv.env ${pv_env_size}
 		setenv boot_rev ${pv_trying}
 	fi
 else
@@ -48,11 +56,16 @@ else
 	setenv boot_rev ${pv_rev}
 fi
 
+echo Pantavisor bootargs: "${pv_baseargs} pv_try=${pv_try} pv_rev=${boot_rev} panic=2 ${fdtbootargs} ${configargs} ${localargs}"
+setenv bootargs "${pv_baseargs} pv_try=${pv_try} pv_rev=${boot_rev} panic=2 pv_quickboot ${fdtbootargs} ${configargs} ${localargs}"
+
 if load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${addr_fit} /trails/${boot_rev}/bsp/pantavisor.fit; then
 	echo Successfully loaded pantavisor.fit. booting...
         iminfo ${addr_fit}
 	run findfdt
-	run get_fit_config
+	if test -z "${name_fit_config}"; then
+		setexpr name_fit_config gsub / _ conf-${fdtfile}
+	fi
         bootm ${addr_fit}#${name_fit_config}
 	echo "failed to boot fit ... resetting in 5 sec"
 	sleep 5
@@ -69,8 +82,6 @@ fi
 fdt addr ${fdt_addr_r}
 fdt get value fdtbootargs /chosen bootargs
 
-echo Pantavisor bootargs: "${pv_baseargs} pv_try=${pv_try} pv_rev=${boot_rev} panic=2 ${fdtbootargs} ${configargs} ${localargs}"
-setenv bootargs "${pv_baseargs} pv_try=${pv_try} pv_rev=${boot_rev} panic=2 ${fdtbootargs} ${configargs} ${localargs}"
 echo Pantavisor kernel load: load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${kernel_addr_r} /trails/${boot_rev}/.pv/pv-kernel.img
 load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${kernel_addr_r} /trails/${boot_rev}/.pv/pv-kernel.img
 echo Pantavisor initrd load: load ${devtype} ${pv_mmcdev}:${pv_mmcdata} ${ramdisk_addr_r} /trails/${boot_rev}/.pv/pv-initrd.img
