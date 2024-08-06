75c1fb98dc348ee68440c5bdef13d10a62cdf548
Alexander Sack <asac@pantacor.com>
2024-08-06 12:34:57 +0000

    add action.yml

--- .github/actions/kas-build/entrypoint.sh
+++ .github/actions/kas-build/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-echo Hello
+target=$1
+command=$2
+bbargs=$3
+
+echo Target: $target
+echo Command: $command
+echo BBARGS: $bbargs
+echo Workspace: $GITHUB_WORKSPACE
+
+ls $GITHUB_WORKSPACE/
 
