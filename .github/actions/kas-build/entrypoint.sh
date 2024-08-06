bd62972bf3414305ef37c346058b98325e0bc1b8
Alexander Sack <asac@pantacor.com>
2024-08-06 18:22:56 +0000

    github/actions/kas-build: dont stop if build fails

--- .github/actions/kas-build/entrypoint.sh
+++ .github/actions/kas-build/entrypoint.sh
@@ -19,12 +19,24 @@ ls -la *
 
 sudo chown -R `id -u`:`id -g` .
 
+err=""
 for c in ${KAS_BUILD_ACTION_CONFIG_DIR}/*.yaml; do
 	echo "Building $c"
-	kas build \
+	set +e
+	if kas build \
 		${KAS_BUILD_ACTION_TARGET:+--target $KAS_BUILD_ACTION_TARGET} \
 		${KAS_BUILD_ACTION_COMMAND:+--cmd $KAS_BUILD_ACTION_COMMAND} \
 		$c \
-		${KAS_BUILD_ACTION_BBARGS}
+		${KAS_BUILD_ACTION_BBARGS}; then
+		res=$?
+		if $res; then
+			err="${err:+$err $res}${err:-$res}"
+		fi
+	fi
 done
+
+if test -n "$err"; then
+	echo "Error failed with codes: $err"
+	exit 1
+fi
 
